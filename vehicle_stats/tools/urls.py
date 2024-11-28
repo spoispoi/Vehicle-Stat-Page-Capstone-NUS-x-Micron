12946c9b9a35e2a4ae75c4f898cc41c9dbdfbca6
Ian <ian1125757@gmail.com>
2024-11-28 23:42:09 +0800

    with stats page conneciton

--- vehicle_stats/tools/urls.py
+++ vehicle_stats/tools/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ToolViewSet
+from django.urls import path
+from django.urls import path
+from . import views
 
 router = DefaultRouter()
 router.register(r'tools', ToolViewSet)  # Register ToolViewSet with the router
 
 urlpatterns = [
     path('', include(router.urls)),  # Include all router-generated URLs
+    path('statistics/<str:equip_id>/', views.equipment_statistics, name='equipment_statistics'),
+    
 ]
+    
+
